6273bf5d10dfa6efab4781f85102be98d3883b04
Jayesh Kotkar <117332855+Jayeshkotkar@users.noreply.github.com>
2025-08-27 22:47:09 +0530

    Update foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { addFood, getAllFood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";  // for image upload or storage system
-
+import auth from "../middleware/auth.js";
 const foodRouter = express.Router();
+
 
 // Image Storage System
 const storage = multer.diskStorage({
@@ -10,15 +11,17 @@ const storage = multer.diskStorage({
         cb(null, "uploads/");
     },
     filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}${file.originalname}`);
+        return cb(null, `${Date.now()}-${file.originalname}`);
     }
 });
 
-// multer middleware
+
 const upload = multer({ storage: storage });
+
+
         
 // add food item
-foodRouter.post("/add", upload.single("image"), addFood); 
+foodRouter.post("/add", auth, upload.single("image"), addFood); 
 
 // get all food items
 foodRouter.get("/list", getAllFood);
